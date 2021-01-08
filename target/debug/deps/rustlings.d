ac0dfd30c029374cd0fc051e6b0762d50d301ce4
王子杰 <751483992@qq.com>
2021-01-09 00:58:32 +0800

    Initial commit

---
+++ target/debug/deps/rustlings.d
@@ -0,0 +1,5 @@
+/Users/wangzijie/IdeaProjects/rustlings/target/debug/deps/rustlings: src/main.rs
+
+/Users/wangzijie/IdeaProjects/rustlings/target/debug/deps/rustlings.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rustlings.d
@@ -0,0 +1,1 @@
+/Users/wangzijie/IdeaProjects/rustlings/target/debug/rustlings: /Users/wangzijie/IdeaProjects/rustlings/src/main.rs
